537d92a2a47022649b584350a454ae785a13bdc2
Yusuf Ahmed <163534012+YusufAhmed9@users.noreply.github.com>
2024-11-30 23:28:13 +0200

    add: Top ten series by views with its comparator, edit: All dates are set now to localdate

--- src/main/java/WatChill/Series/Season.java
+++ src/main/java/WatChill/Series/Season.java
@@ -1,28 +1,41 @@
 package WatChill.Series;
 
 import WatChill.Review.Review;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
+// Specify the attributes for jackson and ignore getter methods
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE)
+
 public class Season {
     private String title;
-    private Date releaseDate;
+    private LocalDate releaseDate;
     private String description;
     private ArrayList<Episode> episodes;
 
-    public Season(String title, String description, Date releaseDate, ArrayList<Episode> episodes) {
+    // Specify the constructor and parameters for jackson to serialize rhe class
+    @JsonCreator
+    public Season(
+            @JsonProperty("title") String title,
+            @JsonProperty("description") String description,
+            @JsonProperty("releaseDate") LocalDate releaseDate,
+            @JsonProperty("episodes") ArrayList<Episode> episodes) {
         this.title = title;
         this.description = description;
         this.releaseDate = releaseDate;
         this.episodes = episodes;
     }
 
-    public Date getReleaseDate() {
+    public LocalDate getReleaseDate() {
         return releaseDate;
     }
 
-    public void setReleaseDate(Date releaseDate) {
+    public void setReleaseDate(LocalDate releaseDate) {
         this.releaseDate = releaseDate;
     }
 
@@ -42,7 +55,7 @@ public class Season {
         this.episodes = episodes;
     }
 
-    public void addEpisode(Episode episode){
+    public void addEpisode(Episode episode) {
         episodes.add(episode);
     }
 
@@ -54,10 +67,10 @@ public class Season {
         this.description = description;
     }
 
-    public int getViewsCount(){
+    public int getViewsCount() {
         //Initialize total views count to zero
         int totalViewsCount = 0;
-        for(Episode episode : getEpisodes()){
+        for (Episode episode : getEpisodes()) {
             //Sums all episodes of the season view count
             totalViewsCount += episode.getViewsCount();
         }
--- src/main/java/WatChill/Series/Series.java
+++ src/main/java/WatChill/Series/Series.java
@@ -1,24 +1,46 @@
 package WatChill.Series;
 
+import WatChill.Cast.Cast;
+import WatChill.Director.Director;
 import WatChill.FileHandling.JsonReader;
 import WatChill.FileHandling.JsonWriter;
-import WatChill.Review.Review;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.*;
+
+// Specify the attributes for jackson and ignore getter methods
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE)
 
 public class Series {
     private String id;
     private String title;
-    private Date releaseDate;
+    private LocalDate releaseDate;
     private ArrayList<Season> seasons;
     private String description;
     private ArrayList<String> languages;
     private String country;
     private ArrayList<String> genres;
     private static ArrayList<Series> fileSeries;
+    private ArrayList<Director> directors;
+    private ArrayList<Cast> casts;
 
-    public Series(String id, String title, Date releaseDate, ArrayList<Season> seasons, String description, ArrayList<String> languages, String country, ArrayList<String> genres) {
+    // Specify the constructor and parameters for jackson to serialize rhe class
+    @JsonCreator
+    public Series(
+            @JsonProperty("id") String id,
+            @JsonProperty("title") String title,
+            @JsonProperty("releaseDate") LocalDate releaseDate,
+            @JsonProperty("seasons") ArrayList<Season> seasons,
+            @JsonProperty("description") String description,
+            @JsonProperty("languages") ArrayList<String> languages,
+            @JsonProperty("country") String country,
+            @JsonProperty("genres") ArrayList<String> genres,
+            @JsonProperty("director") ArrayList<Director> directors,
+            @JsonProperty("casts") ArrayList<Cast> casts
+    ) {
         this.id = id;
         this.title = title;
         this.releaseDate = releaseDate;
@@ -27,6 +49,32 @@ public class Series {
         this.languages = languages;
         this.country = country;
         this.genres = genres;
+        this.directors = directors;
+        this.casts = casts;
+    }
+
+    public ArrayList<Director> getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(ArrayList<Director> directors) {
+        this.directors = directors;
+    }
+
+    public void addDirector(Director director){
+        directors.add(director);
+    }
+
+    public ArrayList<Cast> getCasts() {
+        return casts;
+    }
+
+    public void setCasts(ArrayList<Cast> casts) {
+        this.casts = casts;
+    }
+
+    public void addCast(Cast cast){
+        casts.add(cast);
     }
 
     public ArrayList<String> getGenres() {
@@ -37,7 +85,7 @@ public class Series {
         this.genres = genres;
     }
 
-    public void addGenre(String genre){
+    public void addGenre(String genre) {
         genres.add(genre);
     }
 
@@ -57,7 +105,7 @@ public class Series {
         this.languages = languages;
     }
 
-    public void addLanguage(String language){
+    public void addLanguage(String language) {
         languages.add(language);
     }
 
@@ -85,15 +133,15 @@ public class Series {
         this.seasons = seasons;
     }
 
-    public void addSeason(Season season){
+    public void addSeason(Season season) {
         seasons.add(season);
     }
 
-    public Date getReleaseDate() {
+    public LocalDate getReleaseDate() {
         return releaseDate;
     }
 
-    public void setReleaseDate(Date releaseDate) {
+    public void setReleaseDate(LocalDate releaseDate) {
         this.releaseDate = releaseDate;
     }
 
@@ -105,54 +153,83 @@ public class Series {
         this.title = title;
     }
 
-    public int getViewsCount(){
+    public int getViewsCount() {
         //Initialize total views count to zero
         int totalViewsCount = 0;
-        for(Season season : getSeasons()){
+        for (Season season : getSeasons()) {
             //Sums all seasons of the series view count
             totalViewsCount += season.getViewsCount();
         }
         return totalViewsCount;
     }
 
-    public static ArrayList<Series> retrieveSeries(){
-        if(fileSeries == null)//If series is not read yet
+    public static ArrayList<Series> retrieveSeries() {
+        if (fileSeries == null)//If series is not read yet
             return fileSeries = JsonReader.readJsonFile("./src/main/data/Series.json", Series.class);
         return fileSeries;
     }
 
-    private int findSeriesIndex(){
+    private int findSeriesIndex() {
         retrieveSeries();
-        for(int i = 0; i < fileSeries.size(); i++){
+        for (int seriesIndex = 0; seriesIndex < fileSeries.size(); seriesIndex++) {
             //Comparing every series id in the database to current id
-            if(fileSeries.get(i).getId().equals(getId()))//If true then it is already stored
-                return i;
+            if (fileSeries.get(seriesIndex).getId().equals(getId()))//If true then it is already stored
+                return seriesIndex;
         }
         //if -1 then it was not found in database
         return -1;
     }
 
-    public void addCurrentSeriesToList(){
-        if(findSeriesIndex() == -1)//If it wasn't found in database
+    public void addSeries() {
+        if (findSeriesIndex() == -1)//If it wasn't found in database
             fileSeries.add(this);//add to database
         else //Was found "There's a chance it's not updated"
             fileSeries.set(findSeriesIndex(), this);//Update it's value in database
     }
 
-    public static void storeSeries() {//Function that write all series to a json file
+    public static void storeAllSeries() {//Function that write all series to a json file
         JsonWriter.writeJsonToFile("./src/main/data/Series.json", fileSeries);
     }
 
-    public static ArrayList<Series> findSeriesByGenre(String genre){
-        retrieveSeries();
+    public static ArrayList<Series> findByGenre(String genre) {
         //ArrayList to store series with wanted genre
-        ArrayList<Series>desiredSeries = new ArrayList<>();
-        for(Series series : retrieveSeries()){
-            if(series.getGenres().indexOf(genre) == -1)//If the genre is not found inside this series then it won't make it to list
+        ArrayList<Series> desiredSeries = new ArrayList<>();
+        for (Series series : retrieveSeries()) {
+            if (!series.getGenres().contains(genre))//If the genre is not found inside this series then it won't make it to list
                 continue;
             //Adds series with the desired genre
             desiredSeries.add(series);
         }
         return desiredSeries;
+    }
+
+    private double getSeriesAverageViews() {
+        //Total number of views of series
+        int totalViews = getViewsCount();
+        //A variable to count number of episodes in the series
+        double totalEpisodes = 0;
+        for (Season season : getSeasons()) {
+            //Adds number of episodes in each season
+            totalEpisodes += season.getEpisodes().size();
+        }
+        return totalViews / totalEpisodes;
+    }
+
+    static class SeriesComparator implements Comparator<Series> {
+        @Override
+        public int compare(Series s1, Series s2) {//Comparator that sorts series by views in descending order
+            return Double.compare(s2.getSeriesAverageViews(), s1.getSeriesAverageViews());
+        }
+    }
+
+    public static ArrayList<Series> getTopTen() {
+        ArrayList<Series> sortedSeriesByViews = retrieveSeries();
+        //Using a comparator to sort ArrayList by views in descending order
+        sortedSeriesByViews.sort(new SeriesComparator());
+        //Takes top ten series after sorting
+        List<Series> topTenSeries = sortedSeriesByViews.subList(0, Math.min(10, sortedSeriesByViews.size()));
+        // Convert the list to an ArrayList of Series
+        return new ArrayList<>(topTenSeries);
+
     }
 }
