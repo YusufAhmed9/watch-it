6ae30754a9dbb8a66f71d0b5ca21ad2f6e3e1e4a
Yusuf Ahmed <163534012+YusufAhmed9@users.noreply.github.com>
2024-12-19 20:57:22 +0200

    add: adding cast page, initial add series design in admin.

---
+++ src/main/java/WatChill/Crew/AddCrewCardController.java
@@ -0,0 +1,38 @@
+package WatChill.Crew;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class AddCrewCardController {
+    @FXML
+    ImageView addButton;
+    @FXML
+    ImageView crewPicture;
+    @FXML
+    Text crewName;
+
+    private void initializeData(String crewId, Runnable runnable) {
+        Crew crew = Crew.findById(crewId);
+        crewPicture.setImage(new Image(getClass().getResource(crew.getPicture()).toExternalForm()));
+        crewName.setText(crew.getFirstName() + ' ' + crew.getLastName());
+        addButton.setOnMouseClicked(_->{
+            setAddButton();
+            runnable.run();
+        });
+    }
+
+    private void setAddButton(){
+        if(addButton.getImage().getUrl().equals(getClass().getResource("/WatChill/Content/Series/media/plus.png").toExternalForm())){
+            addButton.setImage(new Image(getClass().getResource("/WatChill/Content/Series/media/minus-circle.png").toExternalForm()));
+        }
+        else{
+            addButton.setImage(new Image(getClass().getResource("/WatChill/Content/Series/media/plus.png").toExternalForm()));
+        }
+    }
+
+    public void build(String crewId, Runnable runnable) {
+        initializeData(crewId, runnable);
+    }
+}
